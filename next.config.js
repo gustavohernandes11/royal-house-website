2fa5b1ee9fa27463983652db00ffd011cbd3203c
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-06-01 15:24:06 -0300

    ci: add sass compatibility

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
-const { sizes } = require('./blocks/Image/sizes.json');
+require("dotenv").config();
+const { sizes } = require("./blocks/Image/sizes.json");
 
 module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles"), "/components/*"],
+  },
   publicRuntimeConfig: {
     SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   },
   images: {
     domains: [
-      'localhost',
+      "localhost",
       // Your domain(s) here
     ],
     deviceSizes: sizes,
